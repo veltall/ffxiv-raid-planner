e0715e703e06db42906dd6e4817eb4fb2fc60b89
Vu Dinh <vuniverse143@gmail.com>
2018-04-11 04:46:59 -0700

    cook up a UI demo prototype; implement logic to Ability class, polishing up cooldown overlap presentation

---
+++ lib/ffxiv_ability.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:collection';
+
+class Ability {
+  // app-wide state
+  static final Map<String, SplayTreeMap<int, bool>> _allhistory =
+      new Map<String, SplayTreeMap<int, bool>>();
+
+  // public identity data
+  String name;
+  Duration recast; // in milliseconds
+  Duration duration;
+
+  // local data
+  String _iconPath;
+  SplayTreeMap _history;
+
+  Ability(
+      {@required this.name, @required this.duration, @required this.recast}) {
+    // required named parameters
+    assert(this.name != null);
+    assert(this.duration != null);
+    assert(this.recast != null);
+    // icon's filename has no spaces, apostrophes, and is all lowercase
+    RegExp spaceSelector = new RegExp(r"[\s|']+");
+    final String filename = name.replaceAll(spaceSelector, "").toLowerCase();
+    _iconPath = 'res/images/icons/skills/$filename.png';
+    if (_allhistory[name] == null)
+      _allhistory[name] = new SplayTreeMap<int, bool>();
+    _history = _allhistory[name];
+  }
+
+  bool isValidActivationTime(int now) {
+    bool isDuplicate = _history.containsKey(now);
+    // int prev = _history.lastKeyBefore(now);
+    // int next = _history.firstKeyAfter(now);
+    // int prevDistance = prev == null ? recast.inSeconds : now - prev;
+    // int nextDistance = next == null ? recast.inSeconds : next - now;
+    return isDuplicate
+        ? false
+        : _overlapPrev(now) <= 0 && _overlapNext(now) <= 0;
+  }
+
+  // The result returned by this method can be understood as
+  // "I can use this cooldown starting from `overLap` to the right of now"
+  // e.g. prev = 15, recast = 30, now = 50
+  //      prev + recast - now = -5, meaning I can re-use from 45 at the earliest
+  int _overlapPrev(int now) {
+    int overlap = 0 - now;
+    int prev = _history.lastKeyBefore(now);
+    if (prev != null) {
+      overlap = prev + recast.inSeconds - now;
+    }
+    return overlap;
+  }
+
+  int _overlapNext(int now) {
+    int overlap = now - 720; // enrage at 12 minutes
+    int next = _history.firstKeyAfter(now);
+    print('debug $name next = $next');
+    if (next != null) {
+      overlap = now - (next - recast.inSeconds);
+    }
+    return overlap;
+  }
+
+  void activate(int currentTimeInSeconds) {
+    if (isValidActivationTime(currentTimeInSeconds)) {
+      _history[currentTimeInSeconds] = true;
+    }
+  }
+}
+
+// ------------------------------------------
+
+class AbilityWidget extends StatefulWidget {
+  final Ability ability;
+  final int now;
+
+  AbilityWidget({@required this.ability, @required this.now});
+
+  @override
+  _AbilityWidgetState createState() => new _AbilityWidgetState();
+}
+
+class _AbilityWidgetState extends State<AbilityWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final Ability ability = widget.ability;
+    final int now = widget.now;
+    final int wait = ability._overlapPrev(now);
+    final int hurry = ability._overlapNext(now);
+    print('${ability.name}: now = $now, overlapPrev = $wait, overlapNext = ${ability._overlapNext(now)}');
+    // print('+ building ${widget.ability.name}');
+    return new Container(
+      height: 64.0,
+      child: new Column(
+        children: <Widget>[
+          new CircleAvatar(
+            backgroundImage: new AssetImage(ability._iconPath),
+          ),
+          new Row(
+            children: <Widget>[
+              ability.isValidActivationTime(now)
+                  ? new Text('')
+                  : new Text('${wait.toString()}s'),
+              ability.isValidActivationTime(now)
+                  ? new Icon(Icons.check)
+                  : new Icon(Icons.timer),
+              ability.isValidActivationTime(now)
+                  ? new Text('')
+                  : new Text('${hurry.toString()}s'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // print('- disposing ${widget.ability.name}');
+    super.dispose();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,336 @@
+import 'package:flutter/material.dart';
+import 'ffxiv_ability.dart';
+
+void main() => runApp(new RaidPlannerApp());
+
+const kAsset1 = 'FFVI-Suplex.png';
+const kAsset2 = 'kefka.png';
+
+class RaidPlannerApp extends StatelessWidget {
+  final _title = "FFXIV Raid Planner Tool";
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: _title,
+      theme: new ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(_title),
+        ),
+        drawer: new Drawer(
+          elevation: 12.0,
+          child: new Column(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+                accountEmail: new Text('veltalldev@gmail.com'),
+                accountName: new Text('VD'),
+                margin: EdgeInsets.zero,
+              ),
+              new Container(
+                height: 300.0,
+                width: 300.0,
+                child: new Center(
+                  child: new Text('center of drawer body'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: new MainScreen(),
+        floatingActionButton: new FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor,
+          child: new Icon(Icons.list),
+          elevation: 13.0,
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => new _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> _globalScaffoldKey =
+      new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      padding: const EdgeInsets.only(top: 16.0, left: 8.0),
+      children: <Widget>[
+        // o5s
+        new ListTile(
+          title: new Text('Sigmascape V1.0 (Savage)'),
+          leading: new CircleAvatar(
+            backgroundImage: new AssetImage(kAsset1),
+          ),
+        ),
+        // o6s
+        new ListTile(
+          title: new Text('Sigmascape V2.0 (Savage)'),
+          leading: new CircleAvatar(
+            child: new Text('o6s'),
+          ),
+        ),
+        // o7s
+        new ListTile(
+          title: new Text('Sigmascape V3.0 (Savage)'),
+          leading: new CircleAvatar(
+            child: new Text('o7s'),
+          ),
+        ),
+        // o8s
+        new ListTile(
+          title: new Text('Sigmascape V4.0 (Savage)'),
+          leading: new CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: new AssetImage(kAsset2),
+          ),
+          onTap: () => _goToV4(context),
+        ),
+        new ListTile(
+          title: new Text('Sigmascape V4.0 (Savage)'),
+          leading: new CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: new AssetImage(kAsset2),
+          ),
+          onTap: () => _goToV4(context),
+        ),
+        new ListTile(
+          title: new Text('Sigmascape V4.0 (Savage)'),
+          leading: new CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: new AssetImage(kAsset2),
+          ),
+          onTap: () => _goToV4(context),
+        ),
+        new ListTile(
+          title: new Text('Sigmascape V4.0 (Savage)'),
+          leading: new CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: new AssetImage(kAsset2),
+          ),
+          onTap: () => _goToV4(context),
+        ),
+      ],
+    );
+  }
+
+  _goToV4(BuildContext context) {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) {
+          return new EncounterScreen(_globalScaffoldKey);
+        },
+      ),
+    );
+  }
+}
+
+// ----------------------------------------------
+
+class EncounterScreen extends StatefulWidget {
+  final GlobalKey<ScaffoldState> _globalScaffoldKey;
+  EncounterScreen(this._globalScaffoldKey);
+  
+
+  @override
+  _EncounterScreenState createState() => new _EncounterScreenState();
+}
+
+class _EncounterScreenState extends State<EncounterScreen> {
+  GlobalKey<ScaffoldState> _scaffoldKey;
+  List<String> events;
+  VoidCallback _showBottomSheetCallback;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaffoldKey = widget._globalScaffoldKey;
+    // _showBottomSheetCallback = _showBottomSheet;
+    events = new List.generate(140, (i) => 'event #${i*3}');
+  }
+
+  void _showBottomSheet(int i) {
+    setState(() {
+      _showBottomSheetCallback = null;
+    });
+    _scaffoldKey.currentState
+        .showBottomSheet<Null>((BuildContext context) {
+          final ThemeData themeData = Theme.of(context);
+          return new Container(
+            decoration: new BoxDecoration(
+              border: new Border(
+                top: new BorderSide(color: themeData.disabledColor),
+              ),
+            ),
+            child: new Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    new AbilityWidget(
+                      ability: new Ability(
+                        name: 'Rampart',
+                        duration: new Duration(seconds: 20),
+                        recast: new Duration(seconds: 90),
+                      )..activate(25),
+                      now: i,
+                    ),
+                    new AbilityWidget(
+                      ability: new Ability(
+                        name: 'Raw Intuition',
+                        duration: new Duration(seconds: 20),
+                        recast: new Duration(seconds: 90),
+                      )..activate(45),
+                      now: i,
+                    ),
+                    new AbilityWidget(
+                      ability: new Ability(
+                        name: 'Convalescence',
+                        duration: new Duration(seconds: 20),
+                        recast: new Duration(seconds: 120),
+                      )..activate(45),
+                      now: i,
+                    ),
+                    new AbilityWidget(
+                      ability: new Ability(
+                        duration: new Duration(seconds: 6),
+                        recast: new Duration(seconds: 180),
+                        name: "Holmgang",
+                      )..activate(10),
+                      now: i,
+                    ),
+                  ],
+                )),
+          );
+        })
+        .closed
+        .whenComplete(() {
+          if (mounted) {
+            setState(() {
+              // re-enable the button
+              // _showBottomSheetCallback = _showBottomSheet;
+            });
+          }
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      key: _scaffoldKey,
+      appBar: new AppBar(
+        title: new Text('o8s'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.share),
+            onPressed: () => print('pressed Share'),
+          ),
+        ],
+        bottom: new PreferredSize(
+          child: new LinearProgressIndicator(
+            value: 0.23,
+            // backgroundColor: Colors.grey,
+          ),
+          preferredSize: Size.zero,
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        // onPressed: () {
+        //   if (_showBottomSheetCallback == null) {
+        //     Navigator.push(
+        //       context,
+        //       new MaterialPageRoute(
+        //         builder: (context) {
+        //           return new AlertDialog(
+        //             title: new Text('Edit Event'),
+        //             content: new Text(
+        //                 'You chose to edit the event. This feature will be supported soon.'),
+        //           );
+        //         },
+        //       ),
+        //     );
+        //   } else if (_showBottomSheetCallback != null) {
+        //     _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        //       content: new Text("Please select an event to edit."),
+        //     ));
+        //   }
+        // },
+        onPressed: null,
+        
+        child: new Icon(Icons.filter_list),
+      ),
+      body: new ListView(
+        children: events
+            .map(
+              (event) => new Container(
+                    decoration: new BoxDecoration(
+                        border: new BorderDirectional(
+                            bottom: new BorderSide(color: Colors.grey))),
+                    child: new ListTile(
+                      title: new Text(event),
+                      isThreeLine: true,
+                      subtitle: new Text('tank buster'),
+                      leading: new CircleAvatar(
+                        child: new Text('12:12',
+                            style: new TextStyle(fontSize: 15.0)),
+                        backgroundColor: Colors.black,
+                      ),
+                      trailing: new Column(
+                        children: <Widget>[
+                          new Row(
+                            children: <Widget>[
+                              new Icon(Icons.info),
+                              new Icon(Icons.home),
+                              new Icon(Icons.wifi),
+                              new Icon(Icons.gps_fixed),
+                              new Icon(Icons.update),
+                              new Icon(Icons.unfold_less),
+                            ],
+                          ),
+                          new Row(
+                            textDirection: TextDirection.rtl,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              new Icon(Icons.info),
+                              new Icon(Icons.home),
+                              new Icon(Icons.wifi),
+                            ],
+                          )
+                        ],
+                      ),
+                      onTap: () => _showBottomSheet(
+                            int.parse(
+                              event.substring(
+                                event.indexOf(new RegExp(r'[0-9]+'), 0),
+                              ),
+                            ),
+                          ),
+                    ),
+                  ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+// turn data into view
+class RaidEventWidget extends StatelessWidget {
+  final String event;
+  RaidEventWidget(this.event);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
